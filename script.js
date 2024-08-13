49dacdabfc245a216990786a623381ea29c9979f
psyjd14 <psyjd14@exmail.nottingham.ac.uk>
2024-08-13 02:14:03 +0100

    fixed scroll issue by using pagex/y instead

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const hirebut = document.getElementById("hirebut");
 
 
 document.addEventListener("mousemove", function(event) {
-    hirebut.style.left = event.clientX - hirebut.offsetWidth/2 + "px";
-    hirebut.style.top = event.clientY - hirebut.offsetHeight/2 + "px";
+    hirebut.style.left = event.pageX - hirebut.offsetWidth/2 + "px";
+    hirebut.style.top = event.pageY - hirebut.offsetHeight/2 + "px";
 });
